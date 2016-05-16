b63f8d5308f32d1c9f671236a59fd1606d4e3eee
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-05-16 13:15:01 +0100

    Committing missing file

---
+++ MyLibrary/ProgressBarList/IProgressBarListItem.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLibrary.ProgressBarList
+{
+    public interface IProgressBarListItem
+    {
+        string Name { get; set; }
+        string ImageName { get; set; }
+        int MinValue { get; set; }
+        int Value { get; set; }
+        int MaxValue { get; set; }
+    }
+}
+
