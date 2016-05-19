fb0a35fdd9de80d80226d868d9e72089cd23d9ad
Sam Greaves <samgreaves0@gmail.com>
2016-05-19 06:01:26 +0100

    Added a total progress bar

--- MyLibrary/ProgressBarListUC.xaml.cs
+++ MyLibrary/ProgressBarListUC.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using MyLibrary.ProgressBarList;
 
 namespace MyLibrary
@@ -29,10 +28,18 @@ namespace MyLibrary
         // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TitleProperty =
             DependencyProperty.Register("Title", typeof(string), typeof(ProgressBarListUc), null);
-        
+
+        private int _total;
+
+        public int Total
+        {
+            get { return _total; }
+            set { _total = Total; }
+        }
+
         public ProgressBarListUc()
         {
-            InitializeComponent();
+            InitializeComponent();           
         }
     }
 }
