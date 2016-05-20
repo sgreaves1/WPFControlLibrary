3e94229f5dc2310ff66cfe765c6aa1a4dec80120
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-05-20 14:37:43 +0100

    added media controls files

---
+++ MyLibrary/MediaControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyLibrary
+{
+    /// <summary>
+    /// Interaction logic for MediaControl.xaml
+    /// </summary>
+    public partial class MediaControl : UserControl
+    {
+        public MediaControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
