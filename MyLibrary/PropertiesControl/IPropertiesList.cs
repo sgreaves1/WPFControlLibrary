bddd09691d7145fa73fc224d7585a45490776185
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-06-09 10:09:40 +0100

    added a list of properties to the properties list class

--- MyLibrary/PropertiesControl/IPropertiesList.cs
+++ MyLibrary/PropertiesControl/IPropertiesList.cs
@@ -1,7 +1,11 @@
-﻿namespace MyLibrary.PropertiesControl
+﻿using System.Collections.ObjectModel;
+
+namespace MyLibrary.PropertiesControl
 {
     public interface IPropertiesList
     {
         string Name { get; set; }
+        
+        ObservableCollection<IProperty> PropertyList { get; set; } 
     }
 }
