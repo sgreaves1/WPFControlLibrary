f9235cd63446948ba664518ec934e24170ba3df8
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-06-27 13:42:57 +0100

    added async method to rewind video

--- LibrarySamples/Pages/MediaControl/MediaControlPage.xaml.cs
+++ LibrarySamples/Pages/MediaControl/MediaControlPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using LibrarySamples.Pages.MediaControl.ViewModel;
 
@@ -11,6 +13,10 @@ namespace LibrarySamples.Pages.MediaControl
     {
         private MediaControlViewModel _viewModel;
 
+        private CancellationTokenSource _source;
+
+        private TimeSpan RewindSpeed = new TimeSpan(0, 0, 0, 1);
+
         public MediaControlPage()
         {
             InitializeComponent();
@@ -20,34 +26,34 @@ namespace LibrarySamples.Pages.MediaControl
 
             ViewModel.StopRequested += (sender, args) =>
             {
+                _source?.Cancel();
+
                 VideoPlayer.Close();
             };
 
             ViewModel.RewindRequested += (sender, args) =>
             {
-                if (VideoPlayer.SpeedRatio >= 0)
-                {
-                    VideoPlayer.SpeedRatio = -1;
-                }
-                else
-                {
-                    VideoPlayer.SpeedRatio = VideoPlayer.SpeedRatio * 2;
-                }
+                _source?.Cancel();
+
+                VideoPlayer.Pause();
+
+                _source = new CancellationTokenSource();
+               
+                Rewind(_source.Token);
             };
 
             ViewModel.PlayRequested += (sender, args) =>
             {
+                _source?.Cancel();
+
                 VideoPlayer.SpeedRatio = 1;
                 VideoPlayer.Play();
             };
 
             ViewModel.FastForwardRequested += (sender, args) =>
             {
-                //VideoPlayer.Position = VideoPlayer.Position + TimeSpan.FromSeconds(10);
-                //if (VideoPlayer.Position >= VideoPlayer.NaturalDuration.TimeSpan)
-                //{
-                //    VideoPlayer.Position -= TimeSpan.FromMilliseconds(100);
-                //}
+                _source?.Cancel();
+
                 if (VideoPlayer.SpeedRatio <= 0)
                 {
                     VideoPlayer.SpeedRatio = 1;
@@ -60,12 +66,13 @@ namespace LibrarySamples.Pages.MediaControl
 
             ViewModel.EjectRequested += (sender, args) =>
             {
+                _source?.Cancel();
+
                 VideoPlayer.SpeedRatio = 1;
             };
 
             ViewModel.FullScreenRequested += (sender, args) =>
             {
-
             };
 
             ViewModel.UpdateTime += (sender, args) =>
@@ -78,6 +85,29 @@ namespace LibrarySamples.Pages.MediaControl
         {
             get { return _viewModel; }
             set { _viewModel = value; }
+        }
+
+        /// <summary>
+        ///  Run a task that will every now and then update the media element position
+        ///  needs a cancelation token to be cancelled whenever another button is pressed
+        ///  needs a a timer to run
+        ///  when timer hits reverse position of video player by a value
+        ///  increase the value every time the rewind button is hit
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        private async void Rewind(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000);
+
+                // check here in case cancelled while delayed
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+                VideoPlayer.Position = VideoPlayer.Position.Subtract(RewindSpeed);
+            }
         }
     }
 }
