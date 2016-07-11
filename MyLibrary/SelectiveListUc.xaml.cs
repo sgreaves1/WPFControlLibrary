548a9540f8259daa8b428c31a20c74910301c9bc
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-07-11 13:58:33 +0100

    Added available and selected items properties

--- MyLibrary/SelectiveListUc.xaml.cs
+++ MyLibrary/SelectiveListUc.xaml.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using MyLibrary.SelectiveList;
+
 namespace MyLibrary
 {
     /// <summary>
@@ -6,9 +11,48 @@ namespace MyLibrary
     /// </summary>
     public partial class SelectiveListUc 
     {
+        /// <summary>
+        /// Dependency property for the <see cref="AvailibleItems"/> property.
+        /// </summary>
+        public static readonly DependencyProperty AvailibleItemsProperty =
+            DependencyProperty.Register("AvailibleItems", 
+                typeof(IEnumerable<ISelectiveListItem>), 
+                typeof(SelectiveListUc), 
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Dependency property for the <see cref="SelectedItems"/> property.
+        /// </summary>
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register("SelectedItems",
+                typeof(IEnumerable<ISelectiveListItem>),
+                typeof(SelectiveListUc),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public SelectiveListUc()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Items to display in the available items list box
+        /// </summary>
+        public IEnumerable<ISelectiveListItem> AvailibleItems
+        {
+            get { return (IEnumerable<ISelectiveListItem>)GetValue(AvailibleItemsProperty); }
+            set { SetValue(AvailibleItemsProperty, value); }
+        }
+
+        /// <summary>
+        /// Items to display in the selected items list box
+        /// </summary>
+        public IEnumerable<ISelectiveListItem> SelectedItems
+        {
+            get { return (IEnumerable<ISelectiveListItem>)GetValue(SelectedItemsProperty); }
+            set { SetValue(SelectedItemsProperty, value); }
         }
     }
 }
