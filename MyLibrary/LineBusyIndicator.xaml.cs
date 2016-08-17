baf952634dfc8e55a2a965cf332bfb6866fa4212
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-08-17 18:17:24 +0100

    add line busy indicator

---
+++ MyLibrary/LineBusyIndicator.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace MyLibrary
+{
+    /// <summary>
+    /// Interaction logic for LineBusyIndicator.xaml
+    /// </summary>
+    public partial class LineBusyIndicator : UserControl
+    {
+        public LineBusyIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
